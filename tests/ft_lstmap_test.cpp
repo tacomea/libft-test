ce18dd41e937f1d5a6c8ca1c88dc5fd95ddbb491
moromin <34294957+moromin@users.noreply.github.com>
2021-07-20 10:59:21 +0900

    fasdf

--- tests/ft_lstmap_test.cpp
+++ tests/ft_lstmap_test.cpp
@@ -5,6 +5,7 @@ protected:
 	t_list  *test1;
     t_list  *test2;
     t_list  *test3;
+	t_list	*res;
 	virtual void SetUp(){
 	}
 	virtual void TearDown(){
@@ -12,12 +13,25 @@ protected:
 	}
 };
 
+void	*lstiter_func(void *content)
+{
+	unsigned char	*str = (unsigned char *)content;
+
+	while (*str)
+	{
+		if ('a' <= *str && *str <= 'z')
+			*str = 'A' + (*str - 'a');
+		str++;
+	}
+}
+
 TEST_F(lstmap_class, lstmap) {
     test1 = ft_lstnew(strdup("42tokyo"));
     test2 = ft_lstnew(strdup("Cursus"));
-    test3 = ft_lstnew(strdup("Piscine"));
+    test3 = ft_lstnew(strdup("  hoge -- foo\t++ 3"));
     ft_lstadd_back(&test1, test2);
     ft_lstadd_back(&test1, test3);
+	res = ft_lstmap(test1, )
     EXPECT_STREQ("42tokyo", (char *)ft_lstlast(test1)->content);
     EXPECT_EQ(NULL, ft_lstlast(test1)->next);
 }
