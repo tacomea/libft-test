23dfda50eb955c52ba55aab04ce141b724aa68e4
takumihara <takumihara1226@gmail.com>
2021-07-20 11:35:38 +0900

    added lstclear

--- tests/ft_lstclear_test.cpp
+++ tests/ft_lstclear_test.cpp
@@ -1,4 +1,25 @@
-//
-// Created by Takumi Hara on 2021/07/19.
-//
+#include "tests.h"
 
+class lstclear_class : public ::testing::Test {
+protected:
+	virtual void SetUp(){
+	}
+	virtual void TearDown(){
+
+	}
+};
+
+TEST_F(lstclear_class, lstclear) {
+
+	t_list  *test1 = ft_lstnew(malloc(8));
+	ft_lstadd_back(&test1, ft_lstnew(malloc(8)));
+	ft_lstadd_back(&test1, ft_lstnew(malloc(8)));
+	ft_lstadd_back(&test1, ft_lstnew(malloc(8)));
+
+	ft_lstclear(&test1, free);
+	EXPECT_EQ(NULL, test1);
+	//	free(test1);
+	test1 = 0;
+	LEAKS_CONFIRM
+	//	system("leaks a.out");
+}
