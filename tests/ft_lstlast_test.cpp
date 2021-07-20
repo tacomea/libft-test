b029e7f4c64b2e8fd1b8f397dc2cd9404bcaf16b
moromin <34294957+moromin@users.noreply.github.com>
2021-07-20 10:18:46 +0900

    Add lstiter & modify free

--- tests/ft_lstlast_test.cpp
+++ tests/ft_lstlast_test.cpp
@@ -4,7 +4,6 @@ class lstlast_class : public ::testing::Test {
 protected:
 	t_list  *test1;
     t_list  *test2;
-    char    content[STRSIZE];
 	virtual void SetUp(){
 	}
 	virtual void TearDown(){
@@ -13,10 +12,8 @@ protected:
 };
 
 TEST_F(lstlast_class, lstlast) {
-	strcpy(content, "42tokyo");
-    test1 = ft_lstnew(content);
-    strcpy(content, "Cursus");
-    test2 = ft_lstnew(content);    
+    test1 = ft_lstnew((void *)"42tokyo");
+    test2 = ft_lstnew((void *)"Cursus");    
     ft_lstadd_front(&test1, test2);
     EXPECT_STREQ("42tokyo", (char *)ft_lstlast(test1)->content);
     EXPECT_EQ(NULL, ft_lstlast(test1)->next);
