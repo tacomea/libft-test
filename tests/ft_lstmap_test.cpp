3fc3309351db6ddf9767afa01e3c76fec1cfb3d8
moromin <34294957+moromin@users.noreply.github.com>
2021-07-20 10:53:30 +0900

    add lstmap

--- tests/ft_lstmap_test.cpp
+++ tests/ft_lstmap_test.cpp
@@ -1,4 +1,24 @@
-//
-// Created by Takumi Hara on 2021/07/19.
-//
+#include "tests.h"
+
+class lstmap_class : public ::testing::Test {
+protected:
+	t_list  *test1;
+    t_list  *test2;
+    t_list  *test3;
+	virtual void SetUp(){
+	}
+	virtual void TearDown(){
+		LEAKS_CONFIRM
+	}
+};
+
+TEST_F(lstmap_class, lstmap) {
+    test1 = ft_lstnew(strdup("42tokyo"));
+    test2 = ft_lstnew(strdup("Cursus"));
+    test3 = ft_lstnew(strdup("Piscine"));
+    ft_lstadd_back(&test1, test2);
+    ft_lstadd_back(&test1, test3);
+    EXPECT_STREQ("42tokyo", (char *)ft_lstlast(test1)->content);
+    EXPECT_EQ(NULL, ft_lstlast(test1)->next);
+}
 
